c3de2d37ae005de796437fef8c7740281abce578
$+j <lust4life.jun@gmail.com>
2016-06-13 21:27:21 +0800

    use select token

--- WebApiProxy.Tasks/Infrastructure/ConsulHelper.cs
+++ WebApiProxy.Tasks/Infrastructure/ConsulHelper.cs
@@ -19,15 +19,6 @@ namespace WebApiProxy.Tasks.Infrastructure
 
         public static ServiceInfo DiscoveryService(string consulEndpoint, string serviceName,string tag=null)
         {
-            if (serviceName == "traefik")
-            {
-                return new ServiceInfo()
-                       {
-                           Address = "192.168.200.22",
-                           Port = "8090",
-                           ServiceName = serviceName
-                       };
-            }
             using (var client = new HttpClient())
             {
                 var traefikUrl = string.Format("{0}/v1/health/service/{1}", consulEndpoint, serviceName);
@@ -40,18 +31,18 @@ namespace WebApiProxy.Tasks.Infrastructure
                 var traefikInfo = client.GetAsync(traefikUrl).Result.Content.ReadAsAsync<JArray>().Result;
                 foreach (var info in traefikInfo)
                 {
-                    var serviceIsOk = info["Checks"].Any(item => item.Value<string>("Name") == serviceName
+                    var serviceIsOk = info["Checks"].Any(item => item.Value<string>("CheckID") == "service:"+serviceName
                                                          && item.Value<string>("Status") == "passing");
                     if (!serviceIsOk)
                     {
                         continue;
                     }
 
-                    var port = info.Value<string>("Service.Port");
-                    var address = info.Value<string>("Service.Address");
+                    var port = info.SelectToken("Service.Port").Value<string>();
+                    var address = info.SelectToken("Service.Address").Value<string>();
                     if (string.IsNullOrEmpty(address))
                     {
-                        address = info.Value<string>("Node.Address");
+                        address = info.SelectToken("Node.Address").Value<string>();
                     }
 
                     var serviceInfo = new ServiceInfo
